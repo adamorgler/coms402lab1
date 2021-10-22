fa1672f58697e72e5b4149fcc251f9dc4585a3f3
aorgler <aorgler@iastate.edu>
2021-10-22 15:47:33 -0500

    done

--- src/main/java/NodeComp.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class NodeComp implements Comparator<Node> {
-    @Override
-    public int compare(Node n1, Node n2) {
-        return Integer.compare(n1.getPriority(), n2.getPriority());
-    }
-}
